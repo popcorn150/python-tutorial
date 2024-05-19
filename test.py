b21ebb3d80b9f5340d79e325c2e8145f49541fff
popcorn150 <hungrymanchannel@gmail.com>
2024-05-19 17:53:17 +0100

    testing out my reflexes

---
+++ test.py
@@ -0,0 +1,25 @@
+# Variables and typeof with python
+name = "Oluchi"
+print(name)
+print(isinstance(name, str))
+
+print(type(name) == int)
+
+age = 25 #integer
+type(age) == int
+print(age)
+
+fraction = 0.1
+type(fraction) == float
+
+favoriteFood = "Rice"; print(favoriteFood)
+
+# Casting with Python
+frankAge = "20"
+
+age = int(frankAge)
+print(age)
+
+fraction = 890428404320.7
+intFraction = int(fraction)
+print(intFraction)
