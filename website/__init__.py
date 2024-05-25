02a979765e1bb126d300d159cca2f345151354be
popcorn150 <hungrymanchannel@gmail.com>
2024-05-25 20:52:41 +0100

    worked on some stuffs, db creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'EYSvJCa;McG_29 8t?CFX#5J5@$LPX'
+    app.config['SQLAlchemy_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+    
     from .views import views
     from .auth import auth
 
