5a510d1002b1fa2c083626561b6499fd0159a24e
popcorn150 <hungrymanchannel@gmail.com>
2024-05-26 12:56:11 +0100

    finished up sign_up and login

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -7,14 +8,19 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'EYSvJCa;McG_29 8t?CFX#5J5@$LPX'
-    app.config['SQLAlchemy_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefex='/') 
-    app.register_blueprint(auth, url_prefex='/') 
+    app.register_blueprint(auth, url_prefex='/')
+
+    from .db_modules import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
