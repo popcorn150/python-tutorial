d1e40b657c7db60da0cd2de6b7f5b8c6a8cc59a4
popcorn150 <hungrymanchannel@gmail.com>
2024-05-27 19:09:23 +0100

    Flask & python - Note Project

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,7 +11,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'EYSvJCa;McG_29 8t?CFX#5J5@$LPX'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
 
     from .views import views
     from .auth import auth
@@ -23,4 +23,12 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
--- website/auth.py
+++ website/auth.py
@@ -21,10 +21,12 @@ def login():
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist!', category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -55,4 +57,4 @@ def sign_up():
             flash('Account created!.', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", boolean="True")
+    return render_template("sign_up.html", user=current_user)
